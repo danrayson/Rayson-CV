e4ed497fc9492872809dcfbcc3b06b645c799d23
Daniel William Frederick Rayson <danrayson@hotmail.co.uk>
2026-03-19 05:39:46 +0000

    localStorage for userCorrelationid to persist longer

--- Rayson.CV/UI/src/utils/correlation.ts
+++ Rayson.CV/UI/src/utils/correlation.ts
@@ -1,20 +1,14 @@
 const USER_CORRELATION_KEY = 'rayson_cv_user_correlation_id';
-const CORRELATION_KEY = 'rayson_cv_correlation_id';
 
 export function getUserCorrelationId(): string {
-  let id = sessionStorage.getItem(USER_CORRELATION_KEY);
+  let id = localStorage.getItem(USER_CORRELATION_KEY);
   if (!id) {
     id = crypto.randomUUID();
-    sessionStorage.setItem(USER_CORRELATION_KEY, id);
+    localStorage.setItem(USER_CORRELATION_KEY, id);
   }
   return id;
 }
 
 export function getCorrelationId(): string {
-  let id = sessionStorage.getItem(CORRELATION_KEY);
-  if (!id) {
-    id = crypto.randomUUID();
-    sessionStorage.setItem(CORRELATION_KEY, id);
-  }
-  return id;
+  return crypto.randomUUID();
 }
